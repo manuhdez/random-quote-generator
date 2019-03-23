8526b9ae1c870afc99c5e98e25d5855de8adfbe4
manuhdez <manu.hdez.dev@gmail.com>
2019-03-23 20:43:18 +0000

    Updates

---
+++ precache-manifest.3c1e8a63bc023bf1d2e0f054cefb3519.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc72926ceca6d6b6ef3b",
+    "url": "/random-quote-with-hooks/static/js/runtime~main.8bbfb115.js"
+  },
+  {
+    "revision": "be3ba4b6a802c12d0abb",
+    "url": "/random-quote-with-hooks/static/js/main.9fb53276.chunk.js"
+  },
+  {
+    "revision": "ebee9e6ba35e5cd571ce",
+    "url": "/random-quote-with-hooks/static/js/2.59634610.chunk.js"
+  },
+  {
+    "revision": "be3ba4b6a802c12d0abb",
+    "url": "/random-quote-with-hooks/static/css/main.49bfb463.chunk.css"
+  },
+  {
+    "revision": "16aae180380d891f3b19906c29d15a51",
+    "url": "/random-quote-with-hooks/index.html"
+  }
+];
